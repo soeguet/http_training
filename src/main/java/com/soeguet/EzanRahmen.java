c36f77263a0b91f658aa0e4c310ee7678ac8dc82
soeguet <osman_s@live.de>
2022-11-12 14:37:32 +0100

    zweiter push

--- src/main/java/com/soeguet/EzanRahmen.java
+++ src/main/java/com/soeguet/EzanRahmen.java
@@ -12,20 +12,27 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.HashMap;
 
 public class EzanRahmen extends JFrame {
 
     private JPanel hauptPanel;
-    private JComboBox datumComboBox;
+    private JComboBox<String> datumComboBox;
+    private JLabel sabahLabel;
+    private JLabel ogleLabel;
+    private JLabel ikindiLabel;
+    private JLabel aksamLabel;
+    private JLabel yatsiLabel;
+    private JLabel nochTimeLabel;
 
     public EzanRahmen() throws HeadlessException {
-
-        setContentPane(hauptPanel);
-        setSize(600,100);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setVisible(true);
 
 
         HttpClient httpClient = HttpClient.newHttpClient();
@@ -65,14 +72,81 @@ public class EzanRahmen extends JFrame {
 
         datumComboBox.setModel(defaultComboBoxModel);
 
-        datumComboBox.addActionListener(new ActionListener() {
 
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
+        setContentPane(hauptPanel);
+        pack();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setTitle("Bremen - Namaz Vakitleri");
+        setVisible(true);
 
-                System.out.println("ACTION!");
 
+
+        datumComboBox.addActionListener(actionEvent -> {
+
+
+            String sabah = hashMap.get(datumComboBox.getEditor().getItem().toString()).getGunes();
+            String ogle = hashMap.get(datumComboBox.getEditor().getItem().toString()).getOgle();
+            String ikindi = hashMap.get(datumComboBox.getEditor().getItem().toString()).getIkindi();
+            String aksam = hashMap.get(datumComboBox.getEditor().getItem().toString()).getAksam();
+            String yatsi = hashMap.get(datumComboBox.getEditor().getItem().toString()).getYatsi();
+
+            System.out.println("LocalTime.parse(sabah) = " + LocalTime.parse(sabah));
+
+            System.out.println(LocalTime.now().compareTo(LocalTime.parse(sabah)));
+            System.out.println(LocalTime.now().compareTo(LocalTime.parse(ogle)));
+            System.out.println(LocalTime.now().compareTo(LocalTime.parse(ikindi)));
+
+            if (LocalTime.now().compareTo(LocalTime.parse(sabah)) < 0) {
+
+                sabahLabel.setBackground(new Color(0x930C191E, true));
+                nochTimeLabel.setText(String.valueOf(LocalTime.now().until(LocalTime.parse(sabah), ChronoUnit.MINUTES)));
+
+            } else if (LocalTime.now().compareTo(LocalTime.parse(ogle)) < 0) {
+
+                ogleLabel.setBackground(new Color(0x930C191E, true));
+                nochTimeLabel.setText(String.valueOf(LocalTime.now().until(LocalTime.parse(ogle), ChronoUnit.MINUTES)));
+
+            } else if (LocalTime.now().compareTo(LocalTime.parse(ikindi)) < 0) {
+
+                ikindiLabel.setBackground(new Color(0x930C191E, true));
+                nochTimeLabel.setText(String.valueOf(LocalTime.now().until(LocalTime.parse(ikindi), ChronoUnit.MINUTES)));
+
+            } else if (LocalTime.now().compareTo(LocalTime.parse(aksam)) < 0) {
+
+                aksamLabel.setBackground(new Color(0x930C191E, true));
+                nochTimeLabel.setText(String.valueOf(LocalTime.now().until(LocalTime.parse(aksam), ChronoUnit.MINUTES)));
+
+            } else if (LocalTime.now().compareTo(LocalTime.parse(yatsi)) < 0) {
+
+                yatsiLabel.setBackground(new Color(0x930C191E, true));
+                nochTimeLabel.setText(String.valueOf(LocalTime.now().until(LocalTime.parse(yatsi), ChronoUnit.MINUTES)));
             }
+
+            System.out.println("LocalTime.now().until(LocalTime.parse(ikindi), ChronoUnit.MINUTES) = " + LocalTime.now().until(LocalTime.parse(ikindi), ChronoUnit.MINUTES));
+
+
+            sabahLabel.setText(sabah);
+            ogleLabel.setText(ogle);
+            ikindiLabel.setText(ikindi);
+            aksamLabel.setText(aksam);
+            yatsiLabel.setText(yatsi);
+
+            int delay = 1000; //milliseconds
+            ActionListener taskPerformer = new ActionListener() {
+                public void actionPerformed(ActionEvent evt) {
+                    String date = new java.text.SimpleDateFormat("HH:mm:ss").format(new java.util.Date(System.currentTimeMillis()));
+                    nochTimeLabel.setText(date);
+                }
+            };
+            new Timer(delay, taskPerformer).start();
+
+
+
+
+            setSize(getWidth()+1,getHeight()+1);
+            pack();
+
         });
     }
 }
--- src/main/java/com/soeguet/Main.java
+++ src/main/java/com/soeguet/Main.java
@@ -1,17 +1,13 @@
 package com.soeguet;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import com.formdev.flatlaf.intellijthemes.FlatArcOrangeIJTheme;
+import com.formdev.flatlaf.intellijthemes.FlatXcodeDarkIJTheme;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        FlatXcodeDarkIJTheme.setup();
 
         new EzanRahmen();
 
