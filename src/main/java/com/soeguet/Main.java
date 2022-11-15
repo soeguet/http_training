6c29e0abf9aa0e4cbb1397dd68048b60852c9cc1
soeguet <osman_s@live.de>
2022-11-15 23:06:21 +0100

    zweiter push - zwei modi

--- src/main/java/com/soeguet/Main.java
+++ src/main/java/com/soeguet/Main.java
@@ -15,6 +15,8 @@ public class Main extends JFrame {
 
         FlatXcodeDarkIJTheme.setup();
 
+
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setTitle("Bremen - Namaz Vakitleri");
